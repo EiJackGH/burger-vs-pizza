cd5517f97e1a8af92279ae2a808ff7da7c51737f
EiJackGH <rahmatnaim.48@gmail.com>
2026-06-29 20:25:34 +0800

    Burger vs. Pizza New Update

--- main.ts
+++ main.ts
@@ -1,7 +1,21 @@
+function gameOver () {
+    game.setGameOverMessage(true, "Player 1 Wins!")
+    game.setGameOverMessage(true, "Player 2 Wins!")
+    game.setGameOverScoringType(game.ScoringType.HighScore)
+}
+controller.player2.onEvent(ControllerEvent.Connected, function () {
+    controller.moveSprite(pizza)
+})
+controller.player1.onEvent(ControllerEvent.Connected, function () {
+    controller.moveSprite(burger)
+})
+let pizza: Sprite = null
+let burger: Sprite = null
 game.splash("Burger vs. Pizza")
-let burger = sprites.create(assets.image`burger`, SpriteKind.Player)
+burger = sprites.create(assets.image`burger`, SpriteKind.Player)
 burger.left = 0
-let pizza = sprites.create(assets.image`pizza`, SpriteKind.Player)
+pizza = sprites.create(assets.image`pizza`, SpriteKind.Player)
 pizza.right = scene.screenWidth()
 controller.player1.moveSprite(burger)
 controller.player2.moveSprite(pizza)
+info.startCountdown(30)
