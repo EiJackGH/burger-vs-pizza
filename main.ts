10f9cb378e48740cb5bfaec07027b1d3a2e938f5
EiJackGH <rahmatnaim.48@gmail.com>
2026-06-30 15:06:19 +0800

    New bugfix

--- main.ts
+++ main.ts
@@ -3,19 +3,11 @@ function gameOver () {
     game.setGameOverMessage(true, "Player 2 Wins!")
     game.setGameOverScoringType(game.ScoringType.HighScore)
 }
-controller.player2.onEvent(ControllerEvent.Connected, function () {
-    controller.moveSprite(pizza)
-})
-controller.player1.onEvent(ControllerEvent.Connected, function () {
-    controller.moveSprite(burger)
-})
-let pizza: Sprite = null
-let burger: Sprite = null
 game.splash("Burger vs. Pizza")
-burger = sprites.create(assets.image`burger`, SpriteKind.Player)
+let burger = sprites.create(assets.image`burger`, SpriteKind.Player)
 burger.left = 0
-pizza = sprites.create(assets.image`pizza`, SpriteKind.Player)
+let pizza = sprites.create(assets.image`pizza`, SpriteKind.Player)
 pizza.right = scene.screenWidth()
-controller.player1.moveSprite(burger)
-controller.player2.moveSprite(pizza)
+controller.moveSprite(burger)
+controller.moveSprite(pizza)
 info.startCountdown(30)
